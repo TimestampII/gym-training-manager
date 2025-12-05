db2978970b43f15b05900edda60b36723f965988
Nikita_Vasilkov <vasilkov.nikitcka@yandex.ru>
2025-12-05 09:52:14 +0300

    Главная ветка старого знакомого владельца зала

---
+++ src/Age.java
@@ -0,0 +1,4 @@
+
+public enum Age {
+    CHILD, ADULT
+}
---
+++ src/CounterOfTrainings.java
@@ -0,0 +1,22 @@
+public class CounterOfTrainings implements Comparable<CounterOfTrainings> {
+    private Coach coach;
+    private int count;
+
+    public CounterOfTrainings(Coach coach, int count) {
+        this.coach = coach;
+        this.count = count;
+    }
+
+    public Coach getCoach() {
+        return coach;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public int compareTo(CounterOfTrainings other) {
+        return Integer.compare(other.count, this.count);
+    }
+}
